9d39ca3525ef9de87a5182432920877ae91c0382
Nidhi Gupta <nidhig631@gmail.com>
2022-08-29 21:02:37 +0530

    done

--- List.py
+++
@@ -1,8 +0,0 @@
-# list is always in [ ]
-# order followed
-# append to add element at the last or end
-# extend add list at the end
-
-
-state_of_america =["Delaware","Pennsylvania","Maryland"]
-print(len(state_of_america))
